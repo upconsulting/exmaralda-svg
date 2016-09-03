2ad03da970183a4a167c08a90d1333147170883a
Julia Damerow <julia.damerow@googlemail.com>
2016-09-03 03:25:27 -0700

    [EXM-2] added zoom in and out button

--- exmaralda/src/org/exmaralda/partitureditor/svgPanel/MouseListener.java
+++ exmaralda/src/org/exmaralda/partitureditor/svgPanel/MouseListener.java
@@ -24,12 +24,13 @@ import org.w3c.dom.svg.SVGLocatable;
 import org.w3c.dom.svg.SVGRect;
 
 public class MouseListener implements EventListener {
-	
+
 	private SVGDocument svgDoc;
 	private List<SVGLocatable> pathElements;
 	private JSVGCanvas canvas;
-	
-	public MouseListener(SVGDocument svgDoc, JSVGCanvas svgCanvas, List<SVGLocatable> pathElements) {
+
+	public MouseListener(SVGDocument svgDoc, JSVGCanvas svgCanvas,
+			List<SVGLocatable> pathElements) {
 		this.svgDoc = svgDoc;
 		this.pathElements = pathElements;
 		this.canvas = svgCanvas;
@@ -39,84 +40,87 @@ public class MouseListener implements EventListener {
 	public void handleEvent(Event evt) {
 		if (evt instanceof DOMMouseEvent) {
 			EventTarget target = evt.getCurrentTarget();
-			
+
 			if (target instanceof SVGImageElement) {
 				int x = ((DOMMouseEvent) evt).getClientX();
 				int y = ((DOMMouseEvent) evt).getClientY();
-				
+
 				AffineTransform at;
-                at = canvas.getViewBoxTransform();
-                if (at != null) {
-                	try {
+				at = canvas.getViewBoxTransform();
+				if (at != null) {
+					try {
 						at = at.createInverse();
 					} catch (NoninvertibleTransformException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
 					}
-                    Point2D p2d =
-                        at.transform(new Point2D.Float(x, y),
-                                     null);
-                    x = Math.round(((float)p2d.getX()));
-                    y = Math.round((float)p2d.getY());
-                }
-				
+					Point2D p2d = at.transform(new Point2D.Float(x, y), null);
+					x = Math.round(((float) p2d.getX()));
+					y = Math.round((float) p2d.getY());
+				}
+
 				SVGRect rect = new SVGOMRect();
 				rect.setHeight(1.0f);
 				rect.setWidth(1.0f);
 				rect.setX(new Float(x));
 				rect.setY(new Float(y));
-			
-				 GraphicsNode gvtRoot = canvas.getCanvasGraphicsNode();
-                 List<?> it = gvtRoot.getRoot().getChildren();
-                 BridgeContext ctx = canvas.getUpdateManager().getBridgeContext();
-                 
-                 List<GraphicsNode> containingNodes = new ArrayList<>();
-                 for (Object o : ((CanvasGraphicsNode)it.get(0)).getChildren()) {
-                	 
-                	 Rectangle2D rect2 = ((GraphicsNode)o).getSensitiveBounds();
-                	 
-                	 AffineTransform inAt = ((GraphicsNode)o).getInverseTransform();
-                	 Point2D p2d =
-                             inAt.transform(new Point2D.Float(x, y),
-                                          null);
-                     x = Math.round(((float)p2d.getX()));
-                     y = Math.round((float)p2d.getY());
-                	     
-                	 if (rect2.getX() <= x && (rect2.getX() + rect2.getWidth() >= x))  {
-                		 if (y >= rect2.getY() && y <= rect2.getY() + rect2.getHeight()) {
-                			 containingNodes.add((GraphicsNode)o);
-                		 }
-                	 }
-                 }
-                 
-                 GraphicsNode smallestNode = null;
-                 if (!containingNodes.isEmpty()) {
-                	 smallestNode = containingNodes.get(0);
-	                 for (GraphicsNode node : containingNodes.subList(1, containingNodes.size())) {
-	                	 if (smallestNode.getSensitiveBounds().contains(node.getSensitiveBounds())) {
-	                		 smallestNode = node;
-	                	 }
-	                 }
-                 }
-                 
-                 String xpointer = getXPath((SVGOMElement)ctx.getElement(smallestNode));
-                 XPointerObservable._instance.setCurrentXPointer(xpointer);
+
+				GraphicsNode gvtRoot = canvas.getCanvasGraphicsNode();
+				List<?> it = gvtRoot.getRoot().getChildren();
+				BridgeContext ctx = canvas.getUpdateManager()
+						.getBridgeContext();
+
+				List<GraphicsNode> containingNodes = new ArrayList<>();
+				for (Object o : ((CanvasGraphicsNode) it.get(0)).getChildren()) {
+
+					Rectangle2D rect2 = ((GraphicsNode) o).getSensitiveBounds();
+
+					AffineTransform inAt = ((GraphicsNode) o)
+							.getInverseTransform();
+					Point2D p2d = inAt.transform(new Point2D.Float(x, y), null);
+					x = Math.round(((float) p2d.getX()));
+					y = Math.round((float) p2d.getY());
+
+					if (rect2.getX() <= x
+							&& (rect2.getX() + rect2.getWidth() >= x)) {
+						if (y >= rect2.getY()
+								&& y <= rect2.getY() + rect2.getHeight()) {
+							containingNodes.add((GraphicsNode) o);
+						}
+					}
+				}
+
+				GraphicsNode smallestNode = null;
+				if (!containingNodes.isEmpty()) {
+					smallestNode = containingNodes.get(0);
+					for (GraphicsNode node : containingNodes.subList(1,
+							containingNodes.size())) {
+						if (smallestNode.getSensitiveBounds().contains(
+								node.getSensitiveBounds())) {
+							smallestNode = node;
+						}
+					}
+				}
+
+				String xpointer = getXPath((SVGOMElement) ctx
+						.getElement(smallestNode));
+				XPointerObservable._instance.setCurrentXPointer(xpointer);
 			}
-			
-			if (target instanceof SVGOMElement && !(target instanceof SVGImageElement)) {
-				String xpointer = getXPath(((SVGOMElement)target));
-                XPointerObservable._instance.setCurrentXPointer(xpointer);
+
+			if (target instanceof SVGOMElement
+					&& !(target instanceof SVGImageElement)) {
+				String xpointer = getXPath(((SVGOMElement) target));
+				XPointerObservable._instance.setCurrentXPointer(xpointer);
 			}
 		}
 	}
-	
-	public String getXPath(SVGOMElement node)
-	{
+
+	public String getXPath(SVGOMElement node) {
 		if (node.hasAttribute("id")) {
 			return "//*[@id='" + node.getAttribute("id") + "']";
 		}
-	    Node parent = node.getParentNode();
-	    return getXPath((SVGOMElement)parent) + "/" + node.getLocalName();
+		Node parent = node.getParentNode();
+		return getXPath((SVGOMElement) parent) + "/" + node.getLocalName();
 	}
 
 }
