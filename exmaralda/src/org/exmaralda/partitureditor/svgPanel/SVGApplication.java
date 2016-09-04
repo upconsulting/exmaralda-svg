20e76c2eb9b651ea6ed52a25aafe8b0b422320d0
Julia Damerow <jdamerow@users.noreply.github.com>
2016-09-04 21:15:15 +0200

    Story/exm 5 (#4)
    * [EXM-5] fixed layout
    
    * [EXM-5] organize imports

--- exmaralda/src/org/exmaralda/partitureditor/svgPanel/SVGApplication.java
+++ exmaralda/src/org/exmaralda/partitureditor/svgPanel/SVGApplication.java
@@ -10,6 +10,9 @@ public class SVGApplication {
     public static void main(String[] args) {
         // Create a new JFrame.
         JFrame f = new JFrame("Batik");
+        f.pack();
+        f.setSize(1000, 600);
+       
         DisplaySVGPanel app = new DisplaySVGPanel(f);
         
         // Add components to the frame.
@@ -21,8 +24,7 @@ public class SVGApplication {
                 System.exit(0);
             }
         });
-        f.setSize(1000, 600);
-        f.setVisible(true);
+         f.setVisible(true);
     }
 
     
